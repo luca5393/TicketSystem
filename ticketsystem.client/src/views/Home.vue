53928aaca5aebd7eec8ce08022101627ad538602
luca5393 <120323430+luca5393@users.noreply.github.com>
2024-11-05 12:34:33 +0100

    added productionList

--- ticketsystem.client/src/views/Home.vue
+++ ticketsystem.client/src/views/Home.vue
@@ -1,16 +1,24 @@
 <script lang="js" setup>
 import TicketsList from '@/components/TicketsList.vue';
+import ProductList from '@/components/ProductList.vue';
 </script>
 
 <template>
     <AppHeader />
     <h1>HOMESCREEN</h1>
-    <TicketsList></TicketsList>
+    <input type="checkbox" v-model="ticketListTab">
+    <ProductList v-if="ticketListTab"/>
+    <TicketsList v-else/>
 </template>         
 
 <script lang="js">
 export default {
   name: 'Home',
+  data() {
+    return {
+      ticketListTab: true,
+    };
+  },
 };
 </script>
 
